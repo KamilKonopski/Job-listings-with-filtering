462027ab2e19ef3bfc1068f615e23ed61d7eee3e
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-16 23:14:50 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <header class="header"></header>
+</template>
+
+<style scoped>
+.header {
+  background: url("./assets/images/bg-header-mobile.svg") no-repeat;
+  background-color: hsl(180, 29%, 50%);
+  height: 23vh;
+  width: 100%;
+}
+</style>
