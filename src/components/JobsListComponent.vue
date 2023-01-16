c38b8195d71face814f1a90b501f64eb52eb2432
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-17 00:17:18 +0100

    add JobListComponent

---
+++ src/components/JobsListComponent.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+	name: "JobsListComponent",
+	props: ["jobs"],
+};
+</script>
+
+<template>
+	<div v-for="job in jobs" :key="job.id">
+		<div>{{ job.role }}</div>
+	</div>
+</template>
+
+<style scoped></style>
