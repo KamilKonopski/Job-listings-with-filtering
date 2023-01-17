4086a5bbaf1d7480ec6b6dbcd112753ede29d45e
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-17 01:24:33 +0100

    add header styles for desktop

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,37 @@
 import JobsListComponent from "./components/JobsListComponent.vue";
 import jsonData from "./data/data.json";
 export default {
-  components: {
-    JobsListComponent,
-  },
-  setup() {
-    const data = jsonData;
+	components: {
+		JobsListComponent,
+	},
+	setup() {
+		const data = jsonData;
 
-    return { data };
-  },
+		return { data };
+	},
 };
 </script>
 >
 
 <template>
-  <header class="header"></header>
-  <main><JobsListComponent :jobs="data" /></main>
+	<header class="header"></header>
+	<main><JobsListComponent :jobs="data" /></main>
 </template>
 
 <style scoped>
 .header {
-  background: url("./assets/images/bg-header-mobile.svg") no-repeat;
-  background-color: hsl(180, 29%, 50%);
-  height: 23vh;
-  width: 100%;
+	background: url("./assets/images/bg-header-mobile.svg") no-repeat;
+	background-color: hsl(180, 29%, 50%);
+	height: 23vh;
+	width: 100%;
+}
+
+@media screen and (min-width: 1024px) {
+	.header {
+		background: url("./assets/images/bg-header-desktop.svg") no-repeat top
+			center;
+		background-color: hsl(180, 29%, 50%);
+		height: 15vh;
+	}
 }
 </style>
