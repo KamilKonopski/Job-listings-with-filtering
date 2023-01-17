d15c9819e9ccd6af82cb4eba25980bf769b946ab
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-17 16:32:27 +0100

    add SingleJobComponent and margin top

---
+++ src/components/JobsList/JobsListComponent.vue
@@ -0,0 +1,20 @@
+<script>
+import SingleJobComponent from "./SingleJobComponent.vue";
+export default {
+	components: { SingleJobComponent },
+	name: "JobsListComponent",
+	props: ["jobs"],
+};
+</script>
+
+<template>
+	<div class="jobs">
+		<SingleJobComponent :jobs="jobs" />
+	</div>
+</template>
+
+<style scoped>
+.jobs {
+	margin-top: 5rem;
+}
+</style>
