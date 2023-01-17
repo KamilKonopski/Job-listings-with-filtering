326eab85b40462c1b98eb16c118246a989f4cc59
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-17 01:58:39 +0100

    add content

--- src/components/JobsListComponent.vue
+++ src/components/JobsListComponent.vue
@@ -7,7 +7,24 @@ export default {
 
 <template>
 	<div v-for="job in jobs" :key="job.id">
-		<div>{{ job.role }}</div>
+		<span>{{ job.company }}</span>
+		<img :src="job.logo" :alt="job.company" />
+		<span v-if="job.new">new!</span>
+		<span v-if="job.featured">featured</span>
+		<span>{{ job.position }}</span>
+		<ul>
+			<li>{{ job.postedAt }}</li>
+			<li>{{ job.contract }}</li>
+			<li>{{ job.location }}</li>
+		</ul>
+		<div>
+			<button v-for="(language, index) in job.languages" :key="index">
+				{{ language }}
+			</button>
+			<button v-for="(tool, index) in job.tools" :key="index">
+				{{ tool }}
+			</button>
+		</div>
 	</div>
 </template>
 
