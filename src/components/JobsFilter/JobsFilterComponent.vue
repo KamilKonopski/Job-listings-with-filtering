68ef1b5bafc7ffd3e7ee1dd000973d89c6a3338e
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-18 03:18:59 +0100

    add template and classes

--- src/components/JobsFilter/JobsFilterComponent.vue
+++ src/components/JobsFilter/JobsFilterComponent.vue
@@ -6,10 +6,19 @@ export default {
 </script>
 
 <template>
-	<div v-for="(text, index) in clickedButton" :key="index">
-		<span>{{ text }}</span>
+	<div class="filtered">
+		<div
+			class="filtered__items"
+			v-for="(text, index) in clickedButton"
+			:key="index"
+		>
+			<span class="filtered__item">{{ text }}</span>
+			<button class="filtered__text-btn">
+				<img src="/images/icon-remove.svg" alt="remove button" />
+			</button>
+		</div>
+		<button class="filtered__clear-btn">Clear</button>
 	</div>
-	<button>Clear</button>
 </template>
 
 <style scoped></style>
