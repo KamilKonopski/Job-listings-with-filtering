fc01aa83a0f22113461d9a9cda2673bc7e0138ef
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-18 02:04:47 +0100

    Rendering filtered jobs
    Conditional rendering filtered jobs or all jobs

--- src/components/JobsList/JobsListComponent.vue
+++ src/components/JobsList/JobsListComponent.vue
@@ -1,15 +1,31 @@
 <script>
+import { ref } from "vue";
 import SingleJobComponent from "./SingleJobComponent.vue";
 export default {
 	components: { SingleJobComponent },
 	name: "JobsListComponent",
 	props: ["jobs"],
+	setup() {
+		let filteredArray = ref([]);
+		function addToFilteredArray(filteredJob) {
+			filteredArray.value = filteredJob;
+			console.log(filteredArray);
+		}
+
+		return { filteredArray, addToFilteredArray };
+	},
 };
 </script>
 
 <template>
-	<div class="jobs">
-		<SingleJobComponent :jobs="jobs" />
+	<div v-if="filteredArray.length" class="jobs">
+		<SingleJobComponent
+			:jobs="filteredArray"
+			:addToFilteredArray="addToFilteredArray"
+		/>
+	</div>
+	<div v-else class="jobs">
+		<SingleJobComponent :jobs="jobs" :addToFilteredArray="addToFilteredArray" />
 	</div>
 </template>
 
