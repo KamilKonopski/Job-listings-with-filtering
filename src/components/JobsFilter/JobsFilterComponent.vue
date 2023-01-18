4f7ad3af9605dc646e5e936b7c5f19bca72f93aa
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-18 03:11:33 +0100

    add filtered jobs component

---
+++ src/components/JobsFilter/JobsFilterComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+	name: "JobsFilterComponent",
+	props: ["jobs", "clickedButton"],
+};
+</script>
+
+<template>
+	<div v-for="(text, index) in clickedButton" :key="index">
+		<span>{{ text }}</span>
+	</div>
+	<button>Clear</button>
+</template>
+
+<style scoped></style>
