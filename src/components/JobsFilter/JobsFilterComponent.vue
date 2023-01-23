010b846e30890c1f2e9f61c334ec51f4ae1b1b64
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-23 21:20:08 +0100

    add position styles

--- src/components/JobsFilter/JobsFilterComponent.vue
+++ src/components/JobsFilter/JobsFilterComponent.vue
@@ -12,17 +12,13 @@ export default {
 
 <template>
 	<div class="filtered">
-		<div
-			class="filtered__items"
-			v-for="(text, index) in clickedButton"
-			:key="index"
-		>
-			<span class="filtered__item">{{ text }}</span>
-			<img
-				@click="removeFromFilteredArray"
-				src="/images/icon-remove.svg"
-				alt="remove button"
-			/>
+		<div class="filtered__items">
+			<span
+				v-for="(text, index) in clickedButton"
+				:key="index"
+				class="filtered__text"
+				>{{ text }}</span
+			>
 		</div>
 		<button @click="clearFilteredJobs" class="filtered__clear-btn">
 			Clear
@@ -30,4 +26,33 @@ export default {
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.filtered {
+	background-color: var(--light-grayish-cyan-ft);
+	border-radius: 5px;
+	display: flex;
+	justify-content: space-between;
+	min-height: 1rem;
+	margin: 0 auto;
+	transform: translateY(-60%);
+	width: 90%;
+}
+
+.filtered__items {
+	display: flex;
+	flex-basis: 60%;
+	flex-wrap: wrap;
+}
+
+.filtered__text {
+	font-size: 1.5rem;
+}
+
+.filtered__clear-btn {
+	border: none;
+	color: var(--very-dark-grayish-cyan);
+	flex-basis: 40%;
+	font-size: 1.5rem;
+	font-weight: 700;
+}
+</style>
